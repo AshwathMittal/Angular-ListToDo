b747704b32131f3a11cfddeb09fbd6fa4604ad29
Ashwath Mittal <68093378+AshwathMittal@users.noreply.github.com>
2021-08-31 14:43:27 +0530

    First Commit

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+// import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  name = "You List Below"
+  @Input() todo: Todo;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter(); 
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter(); 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(todo: Todo){
+    this.todoDelete.emit(todo)
+    
+  }
+  onCheckBox(todo: Todo){
+  console.log(todo);
+    this.todoCheckbox.emit(todo)
+  }
+}
